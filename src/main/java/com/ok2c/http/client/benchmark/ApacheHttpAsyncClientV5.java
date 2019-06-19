2dd318fb1d935c495a80ac30b9e339ccee226291
Oleg Kalnichevski <olegk@apache.org>
2019-06-19 19:56:07 +0200

    Added agents based on Apache HttpClient 5.0-beta4

---
+++ src/main/java/com/ok2c/http/client/benchmark/ApacheHttpAsyncClientV5.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2019 OK2 Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ok2c.http.client.benchmark;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Semaphore;
+
+import org.apache.hc.client5.http.async.methods.AbstractBinResponseConsumer;
+import org.apache.hc.client5.http.async.methods.AsyncRequestBuilder;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
+import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HeaderElements;
+import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.config.H1Config;
+import org.apache.hc.core5.http.nio.AsyncRequestProducer;
+import org.apache.hc.core5.http.nio.entity.FileEntityProducer;
+import org.apache.hc.core5.http2.HttpVersionPolicy;
+import org.apache.hc.core5.http2.config.H2Config;
+import org.apache.hc.core5.reactor.IOReactorConfig;
+import org.apache.hc.core5.util.Timeout;
+import org.apache.hc.core5.util.VersionInfo;
+
+public class ApacheHttpAsyncClientV5 implements HttpAgent {
+
+    private final PoolingAsyncClientConnectionManager mgr;
+    private final CloseableHttpAsyncClient httpclient;
+
+    public ApacheHttpAsyncClientV5() throws Exception {
+        super();
+        this.mgr = PoolingAsyncClientConnectionManagerBuilder.create()
+                .build();
+        this.httpclient = HttpAsyncClients.createMinimal(
+                HttpVersionPolicy.FORCE_HTTP_1,
+                H2Config.DEFAULT,
+                H1Config.custom()
+                        .setBufferSize(8 * 1024)
+                        .setChunkSizeHint(8 * 1024)
+                        .build(),
+                IOReactorConfig.custom()
+                        .setRcvBufSize(8 * 1024)
+                        .setSndBufSize(8 * 1024)
+                        .setSoTimeout(Timeout.ofMinutes(1))
+                        .build(),
+                this.mgr);
+    }
+
+    @Override
+    public void init() {
+        this.httpclient.start();
+    }
+
+    @Override
+    public void shutdown() throws IOException {
+        this.httpclient.close();
+    }
+
+    @Override
+    public Stats execute(final BenchmarkConfig config) throws Exception {
+        this.mgr.setDefaultMaxPerRoute(config.getConcurrency());
+        this.mgr.setMaxTotal(2000);
+        final Stats stats = new Stats(config.getRequests(), config.getConcurrency());
+
+        final URI target = config.getUri();
+        final RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(Timeout.ofMilliseconds(config.getTimeout()))
+                .setResponseTimeout(Timeout.ofMilliseconds(config.getTimeout()))
+                .build();
+
+        final Semaphore semaphore = new Semaphore(config.getConcurrency());
+        for (int i = 0; i < config.getRequests(); i++) {
+            final AsyncRequestBuilder requestBuilder;
+            if (config.getFile() == null) {
+                requestBuilder = AsyncRequestBuilder.get(target);
+            } else {
+                requestBuilder = AsyncRequestBuilder.put(target)
+                        .setEntity(new FileEntityProducer(
+                                config.getFile(),
+                                config.getContentType() != null ? ContentType.parse(config.getContentType()) : null));
+            }
+            if (!config.isKeepAlive()) {
+                requestBuilder.addHeader(HttpHeaders.CONNECTION, HeaderElements.CLOSE);
+            }
+            final AsyncRequestProducer request = requestBuilder.build();
+            final HttpClientContext clientContext = HttpClientContext.create();
+            clientContext.setRequestConfig(requestConfig);
+            semaphore.acquire();
+            this.httpclient.execute(
+                    request,
+                    new BenchmarkResponseConsumer(stats),
+                    clientContext,
+                    new FutureCallback<Void>() {
+
+                        @Override
+                        public void completed(final Void result) {
+                            semaphore.release();
+                        }
+
+                        @Override
+                        public void failed(final Exception ex) {
+                            semaphore.release();
+                        }
+
+                        @Override
+                        public void cancelled() {
+                            semaphore.release();
+                        }
+
+                    });
+        }
+
+        stats.waitFor();
+        return stats;
+    }
+
+    static class BenchmarkResponseConsumer extends AbstractBinResponseConsumer<Void> {
+
+        private final Stats stats;
+
+        private int status;
+        private long contentLen = 0;
+
+        BenchmarkResponseConsumer(final Stats stats) {
+            super();
+            this.stats = stats;
+        }
+
+        @Override
+        protected void start(final HttpResponse response, final ContentType contentType) throws HttpException, IOException {
+            status = response.getCode();
+        }
+
+        @Override
+        protected int capacityIncrement() {
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        protected void data(final ByteBuffer src, final boolean endOfStream) throws IOException {
+            contentLen += src.remaining();
+        }
+
+        @Override
+        public void failed(final Exception ex) {
+            stats.failure(contentLen);
+        }
+
+        @Override
+        protected Void buildResult() {
+            if (this.status == 200) {
+                stats.success(contentLen);
+            } else {
+                stats.failure(contentLen);
+            }
+            return null;
+        }
+
+        @Override
+        public Void getResult() {
+            return null;
+        }
+
+        @Override
+        public void releaseResources() {
+        }
+
+    }
+
+    @Override
+    public String getClientName() {
+        final VersionInfo vinfo = VersionInfo.loadVersionInfo(
+                "org.apache.hc.client5",
+                Thread.currentThread().getContextClassLoader());
+        return "Apache HttpAsyncClient (ver: " + (vinfo != null ? vinfo.getRelease() : VersionInfo.UNAVAILABLE) + ")";
+    }
+
+    public static void main(final String... args) throws Exception {
+        BenchmarkRunner.run(new ApacheHttpAsyncClientV5(), args);
+    }
+
+}
---
+++ src/main/java/com/ok2c/http/client/benchmark/ApacheHttpClientV5.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2019 OK2 Consulting Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ok2c.http.client.benchmark;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import org.apache.hc.client5.http.classic.methods.RequestBuilder;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.DefaultHttpResponseParserFactory;
+import org.apache.hc.client5.http.impl.io.ManagedHttpClientConnectionFactory;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HeaderElements;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.config.CharCodingConfig;
+import org.apache.hc.core5.http.config.H1Config;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.http.io.entity.FileEntity;
+import org.apache.hc.core5.io.CloseMode;
+import org.apache.hc.core5.util.Timeout;
+import org.apache.hc.core5.util.VersionInfo;
+
+public class ApacheHttpClientV5 implements HttpAgent {
+
+    private final PoolingHttpClientConnectionManager mgr;
+    private final CloseableHttpClient httpclient;
+
+    public ApacheHttpClientV5() {
+        super();
+        this.mgr = PoolingHttpClientConnectionManagerBuilder.create()
+                .setConnectionFactory(new ManagedHttpClientConnectionFactory(
+                        H1Config.custom()
+                                .setBufferSize(8 * 1024)
+                                .setChunkSizeHint(8 * 1024)
+                                .build(),
+                        CharCodingConfig.DEFAULT,
+                        DefaultHttpResponseParserFactory.INSTANCE))
+                .build();
+        this.httpclient = HttpClients.createMinimal(this.mgr);
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void shutdown() {
+        this.mgr.close(CloseMode.GRACEFUL);
+    }
+
+    @Override
+    public Stats execute(final BenchmarkConfig config) throws Exception {
+        this.mgr.setMaxTotal(2000);
+        this.mgr.setDefaultMaxPerRoute(config.getConcurrency());
+        this.mgr.setDefaultSocketConfig(SocketConfig.custom()
+                .setSoTimeout(Timeout.ofMicroseconds(config.getTimeout()))
+                .build());
+
+        final Stats stats = new Stats(config.getRequests(), config.getConcurrency());
+        final WorkerThread[] workers = new WorkerThread[config.getConcurrency()];
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new WorkerThread(stats, config);
+        }
+        for (final WorkerThread worker : workers) {
+            worker.start();
+        }
+        for (final WorkerThread worker : workers) {
+            worker.join();
+        }
+        return stats;
+    }
+
+    class WorkerThread extends Thread {
+
+        private final Stats stats;
+        private final BenchmarkConfig config;
+
+        WorkerThread(final Stats stats, final BenchmarkConfig config) {
+            super();
+            this.stats = stats;
+            this.config = config;
+        }
+
+        @Override
+        public void run() {
+            final byte[] buffer = new byte[4096];
+
+            final URI target = config.getUri();
+
+            final HttpHost targetHost = new HttpHost(target.getScheme(), target.getHost(), target.getPort());
+            final RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectTimeout(Timeout.ofMicroseconds(config.getTimeout()))
+                    .setResponseTimeout(Timeout.ofMicroseconds(config.getTimeout()))
+                    .build();
+
+            while (!this.stats.isComplete()) {
+                final RequestBuilder requestBuilder;
+                if (config.getFile() == null) {
+                    requestBuilder = RequestBuilder.get(target);
+                } else {
+                    requestBuilder = RequestBuilder.put(target)
+                            .setEntity(new FileEntity(
+                                    config.getFile(),
+                                    config.getContentType() != null ? ContentType.parse(config.getContentType()) : null));
+                }
+                if (!config.isKeepAlive()) {
+                    requestBuilder.addHeader(HttpHeaders.CONNECTION, HeaderElements.CLOSE);
+                }
+
+                final ClassicHttpRequest request = requestBuilder.build();
+                final HttpClientContext clientContext = HttpClientContext.create();
+                clientContext.setRequestConfig(requestConfig);
+
+                long contentLen = 0;
+                try (final CloseableHttpResponse response = httpclient.execute(targetHost, request, clientContext)) {
+                    final HttpEntity entity = response.getEntity();
+                    if (entity != null) {
+                        final InputStream instream = entity.getContent();
+                        contentLen = 0;
+                        if (instream != null) {
+                            try {
+                                int l;
+                                while ((l = instream.read(buffer)) != -1) {
+                                    contentLen += l;
+                                }
+                            } finally {
+                                instream.close();
+                            }
+                        }
+                    }
+                    if (response.getCode() == 200) {
+                        this.stats.success(contentLen);
+                    } else {
+                        this.stats.failure(contentLen);
+                    }
+                } catch (final IOException ex) {
+                    this.stats.failure(contentLen);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public String getClientName() {
+        final VersionInfo vinfo = VersionInfo.loadVersionInfo(
+                "org.apache.hc.client5",
+                Thread.currentThread().getContextClassLoader());
+        return "Apache HttpClient (ver: " + (vinfo != null ? vinfo.getRelease() : VersionInfo.UNAVAILABLE) + ")";
+    }
+
+    public static void main(final String... args) throws Exception {
+        BenchmarkRunner.run(new ApacheHttpClientV5(), args);
+    }
+
+}
